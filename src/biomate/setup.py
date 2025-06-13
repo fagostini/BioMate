647935c659c4862bfed79c75f694761da2dc7500
Federico Agostini <federico.agostini@scilifelab.se>
2025-06-13 13:53:18 +0200

    fix: Fix logging level setting
    feat: Add `--taint` flag in `blabber` module
    
    feat: Implement 'tainting' of Undetermined files
    
    The undetermined files are contaminated by reads coming from projects on the same lane

--- src/biomate/setup.py
+++ src/biomate/setup.py
@@ -7,11 +7,11 @@ def setup_logging(args: argparse.Namespace) -> None:
     Set up logging based on the verbosity level specified in the arguments.
     """
     if args.verbose:
-        logging.basicConfig(level=logging.DEBUG)
+        logging.getLogger().setLevel(logging.DEBUG)
     elif args.quiet:
-        logging.basicConfig(level=logging.ERROR)
+        logging.getLogger().setLevel(logging.ERROR)
     else:
-        logging.basicConfig(level=logging.INFO)
+        logging.getLogger().setLevel(logging.INFO)
 
     logging.debug(
         "Logging is set up with level: %s", logging.getLevelName(logging.root.level)
