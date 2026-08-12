ada0538afb6395e4e4e792625d46e9ec4579c120
Federico Agostini <federico.agostini@scilifelab.se>
2026-08-12 14:25:45 +0200

    feat: Add more visual outputs to `nspector` module

--- src/biomate/nspector/nspector.py
+++ src/biomate/nspector/nspector.py
@@ -66,6 +66,12 @@ def init_parser(subparsers: argparse._SubParsersAction) -> argparse.ArgumentPars
         help="Path to the input FASTQ file(s) to inspect.",
     )
     parser.add_argument(
+        "--threads",
+        type=int,
+        default=0,
+        help="Number of threads to use for reading FASTQ files (default: 0, which means no extra threads apart from the main thread).",
+    )
+    parser.add_argument(
         "--output",
         type=pathlib.Path,
         default=pathlib.Path("./output"),
@@ -98,6 +104,156 @@ def generate_cycles_filler(data: polars.DataFrame, cycles: int) -> polars.DataFr
     )
 
 
+def plot_n_per_sequence(data: polars.DataFrame, output_path: pathlib.Path) -> None:
+    """Plot the distribution of N counts per sequence."""
+    chart = (
+        data.group_by(["tile", "x", "y"])
+        .agg(polars.len().alias("n_count"))
+        .group_by("n_count")
+        .agg(polars.len().alias("n_sequences"))
+        .plot.bar(
+            x=alt.X("n_count", title="Number of N bases"),
+            y=alt.Y("n_sequences", title="Number of Sequences").scale(type="symlog"),
+        )
+        .properties(
+            width=1600, height=400, title="Distribution of N counts per sequence"
+        )
+    )
+    chart.save(output_path)
+
+
+def plot_n_per_cycle_and_tile(
+    data: polars.DataFrame, output_path: pathlib.Path
+) -> None:
+    """Plot the distribution of N-containing sequences across cycles and tiles."""
+    filler = generate_cycles_filler(data, int(max(data.get_column("cycles").to_list())))
+
+    chart = (
+        data.group_by(["cycles", "tile"])
+        .agg(polars.len().alias("n_sequences"))
+        .join(
+            filler,
+            on=["cycles", "tile"],
+            how="outer",
+        )
+        .with_columns(
+            polars.when(polars.col("n_sequences").is_not_null())
+            .then(polars.col("n_sequences"))
+            .otherwise(polars.col("n_filler"))
+            .alias("n_sequences_filled")
+        )
+        .select(
+            [
+                "cycles_right",
+                "tile_right",
+                polars.col("n_sequences_filled").alias("n_sequences"),
+            ]
+        )
+        .rename({"cycles_right": "cycles", "tile_right": "tile"})
+        .drop_nulls()
+        .with_columns(
+            polars.col("tile")
+            .cast(polars.Utf8)
+            .str.slice(0, 1)
+            .cast(polars.Int16)
+            .alias("surface"),
+            polars.col("tile")
+            .cast(polars.Utf8)
+            .str.slice(1, 1)
+            .cast(polars.Int16)
+            .alias("swath"),
+            polars.col("tile")
+            .cast(polars.Utf8)
+            .str.slice(2, 2)
+            .cast(polars.Int16)
+            .alias("segment"),
+        )
+    )
+
+    height = chart.get_column("segment").n_unique() * 12
+    width = chart.get_column("cycles").n_unique() * 6
+
+    chart = (
+        chart.plot.rect(
+            x=alt.X(
+                "cycles:O", title="Cycle", axis=alt.Axis(values=list(range(0, 301, 5)))
+            ),
+            y=alt.Y("segment:O", title="Tile"),
+            color=alt.Color("n_sequences:Q", title="Number of Sequences")
+            .scale(scheme="viridis")
+            .bin(maxbins=20),
+            stroke=alt.value("white"),
+            column=alt.Column("surface:N").title("Surface"),
+            row=alt.Row("swath:N").title("Swath").header(labelAngle=0),
+        )
+        .configure_axis(
+            labelFontSize=12,
+        )
+        .properties(
+            width=width,
+            height=height,
+            title="Distribution of N-containing sequences across cycles and tiles",
+        )
+    )
+    chart.save(output_path)
+
+
+def plot_individual_tiles(data: polars.DataFrame, output_path: pathlib.Path) -> None:
+    """Plot the distribution of N-containing sequences for individual tiles."""
+    output_path.joinpath("individual_tiles").mkdir(parents=True, exist_ok=True)
+
+    for tile in data.get_column("tile").unique().to_list():
+        source = (
+            data.filter(polars.col("tile") == tile)
+            .group_by(["x", "y"])
+            .agg(polars.len().alias("z"))
+        )
+
+        base = alt.Chart(source)
+        base_bar = base.mark_bar(opacity=0.3, binSpacing=0)
+
+        xscale = alt.Scale(
+            domainMin=min(source.get_column("x").to_list()),
+            domainMax=max(source.get_column("x").to_list()),
+        )
+        yscale = alt.Scale(
+            domainMin=min(source.get_column("y").to_list()),
+            domainMax=max(source.get_column("y").to_list()),
+        )
+
+        points = (
+            base.mark_circle()
+            .encode(
+                alt.X("x").scale(xscale),
+                alt.Y("y").scale(yscale),
+                # color="Species",
+            )
+            .properties(width=500, height=500)
+        )
+
+        top_hist = base_bar.encode(
+            alt.X("x:Q")
+            # when using bins, the axis scale is set through
+            # the bin extent, so we do not specify the scale here
+            # (which would be ignored anyway)
+            .bin(maxbins=200, extent=xscale.domain)
+            .stack(None)
+            .title(""),
+            alt.Y("count()").stack(None).title(""),
+            # alt.Color("Species:N"),
+        ).properties(width=500, height=60)
+
+        right_hist = base_bar.encode(
+            alt.Y("y:Q").bin(maxbins=200, extent=yscale.domain).stack(None).title(""),
+            alt.X("count()").stack(None).title(""),
+            # alt.Color("Species:N"),
+        ).properties(width=60, height=500)
+
+        chart = top_hist & (points | right_hist)
+
+        chart.save(output_path / "individual_tiles" / f"tile_{tile}_n_distribution.png")
+
+
 def main(args: argparse.Namespace) -> None:
     """Main function."""
     setup_logging(args)
@@ -108,7 +264,7 @@ def main(args: argparse.Namespace) -> None:
         records_with_n = []
         has_format_error = False
 
-        with dnaio.open(input_file) as reader:
+        with dnaio.open(input_file, open_threads=args.threads) as reader:
             for record in reader:
                 n_positions = [i for i, c in enumerate(record.sequence) if c == "N"]
                 if n_positions:
@@ -153,63 +309,19 @@ def main(args: argparse.Namespace) -> None:
                 ]
             )
         )
+        data.write_parquet(args.output / f"{input_file.stem}_n_sequences.parquet")
 
         logging.debug(f"Creating charts for file: {input_file}")
 
-        chart = (
-            data.group_by(["tile", "x", "y"])
-            .agg(polars.len().alias("n_count"))
-            .group_by("n_count")
-            .agg(polars.len().alias("n_sequences"))
-            .plot.bar(
-                x=alt.X("n_count", title="Number of N bases"),
-                y=alt.Y("n_sequences", title="Number of Sequences").scale(
-                    type="symlog"
-                ),
-            )
-            .properties(
-                width=1024, height=480, title="Distribution of N counts per sequence"
-            )
+        plot_n_per_sequence(
+            data, args.output / f"{input_file.stem}_n_count_distribution.png"
         )
-        chart.save(args.output / f"{input_file.stem}_n_count_distribution.png")
 
-        filler = generate_cycles_filler(
-            data, int(max(data.get_column("cycles").to_list()))
+        plot_n_per_cycle_and_tile(
+            data,
+            args.output / f"{input_file.stem}_n_distribution_per_cycle_and_tile.png",
         )
-        chart = (
-            data.group_by(["cycles", "tile"])
-            .agg(polars.len().alias("n_sequences"))
-            .join(
-                filler,
-                on=["cycles", "tile"],
-                how="outer",
-            )
-            .with_columns(
-                polars.when(polars.col("n_sequences").is_not_null())
-                .then(polars.col("n_sequences"))
-                .otherwise(polars.col("n_filler"))
-                .alias("n_sequences_filled")
-            )
-            .select(
-                [
-                    "cycles",
-                    "tile",
-                    polars.col("n_sequences_filled").alias("n_sequences"),
-                ]
-            )
-            .plot.line(
-                x=alt.X("cycles", title="Cycle"),
-                y=alt.Y("n_sequences", title="Number of Sequences").scale(
-                    type="symlog"
-                ),
-                color=alt.Color("tile:N", title="Tile"),
-            )
-            .properties(
-                width=800,
-                height=400,
-                title="Distribution of N-containing sequences across cycles and tiles",
-            )
-        )
-        chart.save(args.output / f"{input_file.stem}_n_cycle_tile_distribution.png")
+
+        plot_individual_tiles(data, args.output)
 
     logging.info("Nspector module finished.")
